c4a18e2d8ba2f379bf2f7f9e769728e69fdc279a
aokidai <aokidaisuke91@gmail.com>
2021-03-24 12:44:36 +0700

    up'

--- TKUDDDBT/app/src/main/java/com/example/tkudddbt/mhThongTin.java
+++ TKUDDDBT/app/src/main/java/com/example/tkudddbt/mhThongTin.java
@@ -4,7 +4,6 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Activity;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
@@ -24,62 +23,24 @@ import android.widget.Toast;
 import java.io.Serializable;
 import java.util.ArrayList;
 
-public class MainActivity extends Activity {
-    public static final String DATABASE_NAME = "BaiTapTKDD.db";
+public class mhThongTin extends AppCompatActivity {
+    Button btnThem, btnThoat;
+    ListView lvTT;
+    Spinner spQQ;
+    int poss = -1;
     SQLiteDatabase db;
-    Button btnLoc, btnThem, btnThoat;
-    Spinner spQueQuan;
-    ListView lvQLTT;
     ArrayList<pesion> dsQLTT = new ArrayList<pesion>();
     MyAdapterQLTT adapter;
     ArrayList<quequan> dsQueQuan = new ArrayList<quequan>();
     ArrayAdapter<quequan> adapter2;
     int posselected = -1;
     int posselected2 = -1;
-    public static final int OPEN_QL = 113;
-    public static final int EDIT_QL = 114;
-    public static final int SAVE_QL = 115;
-    private void initDB(){
-        db = openOrCreateDatabase(DATABASE_NAME, MODE_PRIVATE, null);
-        String sql;
-        try{
-            if (!isTableExists(db, "tblQueQuan")){
-                sql = "CREATE TABLE tblQueQuan (idQue INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,";
-                sql += "QueQuan TEXT NOT NULL);";
-                db.execSQL(sql);
-                sql = "insert into tblQueQuan (QueQuan) values ('ALL')";
-                db.execSQL(sql);
-                sql = "insert into tblQueQuan (QueQuan) values ('Binh Duong')";
-                db.execSQL(sql);
-                sql = "insert into tblQueQuan (QueQuan) values ('Dong Nai')";
-                db.execSQL(sql);
-            }
-            if(!isTableExists(db, "tblPersion")){
-                sql = "CREATE TABLE tblPersion (idPersion INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,";
-                sql += "Ten TEXT NOT NULL,";
-                sql += "SoDT TEXT NOT NULL,";
-                sql += "DiaChi TEXT NOT NULL,";
-                sql += "idQue INTEGER );";
-                db.execSQL(sql);
-                sql = "insert into tblPersion (Ten, SoDT, DiaChi, idQue) values ('Du', '0987', 'Vietnam', 2)";
-                db.execSQL(sql);
-            }
-        }catch (Exception ex){
-            Toast.makeText(this, "Khởi tạo dữ liệu lỗi!", Toast.LENGTH_LONG).show();
-        }
+    private void initWedget(){
+        btnThem = (Button) findViewById(R.id.btnTTThem);
+        btnThoat = (Button) findViewById(R.id.btnTTThoat);
+        lvTT = (ListView) findViewById(R.id.lvTT);
+        spQQ = (Spinner) findViewById(R.id.spTTQueQuan);
     }
-    private boolean isTableExists(SQLiteDatabase database, String tableName){
-        Cursor cursor = database.rawQuery("select DISTINCT tbl_name from sqlite_master where tbl_name" + " = '"+tableName+"'", null);
-        if (cursor != null){
-            if(cursor.getCount()>0){
-                cursor.close();
-                return true;
-            }
-            cursor.close();
-        }
-        return false;
-    }
-
     private void getDSQLTT(){
         try{
             dsQLTT.add(new pesion("Tên", "Số DT", "Địa chỉ"));
@@ -92,7 +53,7 @@ public class MainActivity extends Activity {
                 c.moveToNext();
             }
             adapter = new MyAdapterQLTT(this, android.R.layout.simple_list_item_1, dsQLTT);
-            lvQLTT.setAdapter(adapter);
+            lvTT.setAdapter(adapter);
         }catch (Exception ex){
             Toast.makeText(getApplication(), "Lỗi "+ex.getMessage(), Toast.LENGTH_LONG).show();
         }
@@ -108,7 +69,7 @@ public class MainActivity extends Activity {
             }
             adapter2 = new ArrayAdapter<quequan>(this, android.R.layout.simple_spinner_item, dsQueQuan);
             adapter2.setDropDownViewResource(android.R.layout.select_dialog_singlechoice);
-            spQueQuan.setAdapter(adapter2);
+            spQQ.setAdapter(adapter2);
         }catch (Exception ex){
             Toast.makeText(getApplication(), "Lỗi "+ex.getMessage(), Toast.LENGTH_LONG).show();
         }
@@ -152,7 +113,7 @@ public class MainActivity extends Activity {
             case R.id.mnuedit:
                 pesion ps = dsQLTT.get(posselected);
                 Bundle bundle = new Bundle();
-                Intent intent = new Intent(MainActivity.this, EditThongTin.class);
+                Intent intent = new Intent(mhThongTin.this, EditThongTin.class);
                 bundle.putSerializable("ps", (Serializable) ps);
                 intent.putExtra("data", bundle);
                 startActivityForResult(intent, MainActivity.EDIT_QL);
@@ -164,7 +125,6 @@ public class MainActivity extends Activity {
                 return super.onContextItemSelected(item);
         }
     }
-
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
@@ -187,19 +147,34 @@ public class MainActivity extends Activity {
                 break;
         }
     }
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        btnThem = (Button) findViewById(R.id.btnThem);
-        btnThoat = (Button) findViewById(R.id.btnThoat);
-        spQueQuan = (Spinner) findViewById(R.id.spQueQuan);
-        lvQLTT = (ListView) findViewById(R.id.lvQLTT);
-        initDB();
+        setContentView(R.layout.activity_mh_thong_tin);
+        initWedget();
         //getDSQLTT();
         getQueQuan();
-        registerForContextMenu(lvQLTT);
+        registerForContextMenu(lvTT);
+        Intent callerIntent = getIntent();
+        Bundle pack = callerIntent.getBundleExtra("bt");
+        poss = pack.getInt("ds");
+        spQQ.setSelection(poss);
+
+        try{
+            //dsQLTT.add(new pesion("Tên", "Số DT", "Địa chỉ"));
+            db = openOrCreateDatabase(MainActivity.DATABASE_NAME, MODE_PRIVATE, null);
+            Cursor c = db.rawQuery("Select * from tblPersion where idQue " + " = '" + poss +"'", null);
+            //Cursor c = db.query("tblPersion", null, null, null, null, null, null);
+            c.moveToFirst();
+            while (!c.isAfterLast()){
+                dsQLTT.add(new pesion(c.getString(1).toString(), c.getString(2).toString(), c.getString(3).toString()));
+                c.moveToNext();
+            }
+            adapter = new MyAdapterQLTT(getApplication(), android.R.layout.simple_list_item_1, dsQLTT);
+            lvTT.setAdapter(adapter);
+        }catch (Exception ex){
+            Toast.makeText(getApplication(), "Lỗi "+ex.getMessage(), Toast.LENGTH_LONG).show();
+        }
         btnThoat.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -209,53 +184,18 @@ public class MainActivity extends Activity {
         btnThem.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, InsertThongTin.class);
+                Intent intent = new Intent(mhThongTin.this, InsertThongTin.class);
                 startActivityForResult(intent, MainActivity.OPEN_QL);
             }
         });
-        lvQLTT.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+        lvTT.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
             public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
                 posselected = position;
                 return false;
             }
         });
-        spQueQuan.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                posselected2 = -1;
-                posselected2 = position;
-                posselected2 = posselected2 + 1;
-                if (posselected2 == 1)
-                {
-                    getDSQLTT();
-                }
-                else {
-                    dsQLTT.clear();
-                    try{
-                        dsQLTT.add(new pesion("Tên", "Số DT", "Địa chỉ"));
-                        db = openOrCreateDatabase(MainActivity.DATABASE_NAME, MODE_PRIVATE, null);
-                        Cursor c = db.rawQuery("Select * from tblPersion where idQue " + " = '" + posselected2 +"'", null);
-                        //Cursor c = db.query("tblPersion", null, null, null, null, null, null);
-                        c.moveToFirst();
-                        while (!c.isAfterLast()){
-                            dsQLTT.add(new pesion(c.getString(1).toString(), c.getString(2).toString(), c.getString(3).toString()));
-                            c.moveToNext();
-                        }
-                        adapter = new MyAdapterQLTT(getApplication(), android.R.layout.simple_list_item_1, dsQLTT);
-                        lvQLTT.setAdapter(adapter);
-                    }catch (Exception ex){
-                        Toast.makeText(getApplication(), "Lỗi "+ex.getMessage(), Toast.LENGTH_LONG).show();
-                    }
-                }
 
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-
-            }
-        });
 
     }
 }
